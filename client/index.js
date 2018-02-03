f323de7a2ebfb0fc240b05dd6c90e5627641d4bf
Conner Reeves <ConnerReeves@gmail.com>
2018-02-03 12:05:04 -0800

    starting over

--- client/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render((
-  <div>early-payoff</div>
-), document.getElementById('root'));
--- server/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loanRoutes = require('./Loan');
-
-router.use('/loans', loanRoutes);
-
-module.exports = router;
